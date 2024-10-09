c4b1b1a14380a2aa89244d36bc6f4ed35b02c132
Tien <tienheo1306@gmail.com>
2024-10-09 14:57:28 +0700

    update luon

---
+++ CSharp_laptop/DTO/NhanVienDTO.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_laptop.DTO
+{
+    internal class NhanVienDTO
+    {
+        public string ID_NhanVien { get; set; }
+        public string TenNV { get; set; }
+        public string NgaySinh { get; set; }
+        public string SDT { get; set; }
+        public string DiaChi { get; set; }
+        public bool GioiTinh { get; set; }
+        public string CCCD { get; set; }
+        public string Email { get; set; }
+
+        public NhanVienDTO() { }
+
+        public NhanVienDTO(string idNhanVien, string tenNV, string ngaySinh, string sdt, string diaChi, bool gioiTinh, string cccd, string email)
+        {
+            ID_NhanVien = idNhanVien;
+            TenNV = tenNV;
+            NgaySinh = ngaySinh;
+            SDT = sdt;
+            DiaChi = diaChi;
+            GioiTinh = gioiTinh;
+            CCCD = cccd;
+            Email = email;
+        }
+    }
+}
