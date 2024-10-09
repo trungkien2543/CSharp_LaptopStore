a7e104d3eafad8bd638f711db9709a900f61e4f1
duyKhanh026 <117454177+duyKhanh026@users.noreply.github.com>
2024-10-10 00:10:02 +0700

    😺😺

--- CSharp_laptop/GUI/CreateTaiKhoanGUI.cs
+++ CSharp_laptop/GUI/CreateTaiKhoanGUI.cs
@@ -12,8 +12,11 @@ namespace CSharp_laptop.GUI
 {
     public partial class CreateTaiKhoanGUI : Form
     {
-        public CreateTaiKhoanGUI()
+
+        MainForm mainForm;
+        public CreateTaiKhoanGUI(MainForm mainForm)
         {
+            this.mainForm = mainForm;
             InitializeComponent();
         }
 
@@ -39,7 +42,7 @@ namespace CSharp_laptop.GUI
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            label1.Location = new Point(12, 9);
+            label1.Location = new Point(0, -1);
             label1.Name = "label1";
             label1.Size = new Size(180, 37);
             label1.TabIndex = 0;
@@ -48,79 +51,88 @@ namespace CSharp_laptop.GUI
             // label2
             // 
             label2.AutoSize = true;
-            label2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Font = new Font("Segoe UI", 15.75F);
             label2.Location = new Point(24, 66);
             label2.Name = "label2";
-            label2.Size = new Size(129, 21);
+            label2.Size = new Size(177, 30);
             label2.TabIndex = 1;
             label2.Text = "- Tên Đăng Nhập:";
             // 
             // label3
             // 
             label3.AutoSize = true;
-            label3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Font = new Font("Segoe UI", 15.75F);
             label3.Location = new Point(24, 104);
             label3.Name = "label3";
-            label3.Size = new Size(89, 21);
+            label3.Size = new Size(122, 30);
             label3.TabIndex = 2;
             label3.Text = "- Mật Khẩu:";
             // 
             // label4
             // 
             label4.AutoSize = true;
-            label4.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.Font = new Font("Segoe UI", 15.75F);
             label4.Location = new Point(24, 145);
             label4.Name = "label4";
-            label4.Size = new Size(69, 21);
+            label4.Size = new Size(93, 30);
             label4.TabIndex = 3;
             label4.Text = "- Quyền:";
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(159, 66);
+            textBox1.Font = new Font("Segoe UI", 12F);
+            textBox1.Location = new Point(207, 70);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(342, 23);
+            textBox1.Size = new Size(423, 29);
             textBox1.TabIndex = 4;
             textBox1.TextChanged += textBox1_TextChanged;
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(159, 104);
+            textBox2.Font = new Font("Segoe UI", 12F);
+            textBox2.Location = new Point(207, 108);
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(342, 23);
+            textBox2.Size = new Size(423, 29);
             textBox2.TabIndex = 5;
+            textBox2.TextChanged += textBox2_TextChanged;
+            textBox2.PasswordChar = '*';
             // 
             // comboBox1
             // 
+            comboBox1.Font = new Font("Segoe UI", 12F);
             comboBox1.FormattingEnabled = true;
-            comboBox1.Location = new Point(159, 143);
+            comboBox1.Items.AddRange(new object[] { "Nhân viên quèn \U0001f92e", "Admin 😎" });
+            comboBox1.Location = new Point(207, 147);
             comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(341, 23);
+            comboBox1.Size = new Size(423, 29);
             comboBox1.TabIndex = 6;
+            comboBox1.Text = "Nhân viên quèn \U0001f92e";
             // 
             // panel1
             // 
+            panel1.BorderStyle = BorderStyle.FixedSingle;
             panel1.Controls.Add(label5);
-            panel1.Location = new Point(24, 197);
+            panel1.Location = new Point(24, 199);
             panel1.Name = "panel1";
-            panel1.Size = new Size(476, 185);
+            panel1.Size = new Size(606, 271);
             panel1.TabIndex = 7;
             // 
             // label5
             // 
             label5.AutoSize = true;
-            label5.Location = new Point(205, 84);
+            label5.Location = new Point(276, 123);
             label5.Name = "label5";
             label5.Size = new Size(59, 15);
             label5.TabIndex = 0;
             label5.Text = "Drop here";
+            label5.Click += label5_Click;
             // 
             // iconButton1
             // 
             iconButton1.IconChar = FontAwesome.Sharp.IconChar.None;
             iconButton1.IconColor = Color.Black;
             iconButton1.IconFont = FontAwesome.Sharp.IconFont.Auto;
-            iconButton1.Location = new Point(48, 417);
+            iconButton1.Location = new Point(50, 526);
             iconButton1.Name = "iconButton1";
             iconButton1.Size = new Size(105, 34);
             iconButton1.TabIndex = 8;
@@ -132,16 +144,17 @@ namespace CSharp_laptop.GUI
             iconButton2.IconChar = FontAwesome.Sharp.IconChar.None;
             iconButton2.IconColor = Color.Black;
             iconButton2.IconFont = FontAwesome.Sharp.IconFont.Auto;
-            iconButton2.Location = new Point(362, 417);
+            iconButton2.Location = new Point(485, 526);
             iconButton2.Name = "iconButton2";
             iconButton2.Size = new Size(105, 34);
             iconButton2.TabIndex = 9;
             iconButton2.Text = "Cancel";
             iconButton2.UseVisualStyleBackColor = true;
+            iconButton2.Click += iconButton2_Click;
             // 
             // CreateTaiKhoanGUI
             // 
-            ClientSize = new Size(701, 481);
+            ClientSize = new Size(1043, 589);
             Controls.Add(iconButton2);
             Controls.Add(iconButton1);
             Controls.Add(panel1);
@@ -171,6 +184,22 @@ namespace CSharp_laptop.GUI
         private FontAwesome.Sharp.IconButton iconButton2;
 
         private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void iconButton2_Click(object sender, EventArgs e)
+        {
+
+            mainForm.OpenChildForm(new QuanLyTaiKhoanGUI(mainForm));
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
         {
 
         }
--- CSharp_laptop/GUI/QuanLyTaiKhoanGUI.Designer.cs
+++ CSharp_laptop/GUI/QuanLyTaiKhoanGUI.Designer.cs
@@ -29,21 +29,11 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(QuanLyTaiKhoanGUI));
-            dataGridView1 = new DataGridView();
             button2 = new Button();
             button1 = new Button();
             button3 = new Button();
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            listView1 = new ListView();
             SuspendLayout();
-            // 
-            // dataGridView1
-            // 
-            dataGridView1.BackgroundColor = SystemColors.ButtonHighlight;
-            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(12, 12);
-            dataGridView1.Name = "dataGridView1";
-            dataGridView1.Size = new Size(701, 503);
-            dataGridView1.TabIndex = 5;
             // 
             // button2
             // 
@@ -53,7 +43,7 @@
             button2.ForeColor = Color.White;
             button2.Image = (Image)resources.GetObject("button2.Image");
             button2.ImageAlign = ContentAlignment.MiddleLeft;
-            button2.Location = new Point(729, 76);
+            button2.Location = new Point(973, 76);
             button2.Name = "button2";
             button2.Padding = new Padding(10, 0, 0, 0);
             button2.Size = new Size(112, 58);
@@ -70,7 +60,7 @@
             button1.ForeColor = Color.White;
             button1.Image = (Image)resources.GetObject("button1.Image");
             button1.ImageAlign = ContentAlignment.MiddleLeft;
-            button1.Location = new Point(729, 12);
+            button1.Location = new Point(973, 12);
             button1.Name = "button1";
             button1.Padding = new Padding(10, 0, 0, 0);
             button1.Size = new Size(112, 58);
@@ -88,7 +78,7 @@
             button3.ForeColor = Color.White;
             button3.Image = (Image)resources.GetObject("button3.Image");
             button3.ImageAlign = ContentAlignment.MiddleLeft;
-            button3.Location = new Point(729, 140);
+            button3.Location = new Point(973, 140);
             button3.Name = "button3";
             button3.Padding = new Padding(10, 0, 0, 0);
             button3.Size = new Size(112, 58);
@@ -97,25 +87,32 @@
             button3.TextImageRelation = TextImageRelation.ImageBeforeText;
             button3.UseVisualStyleBackColor = false;
             // 
+            // listView1
+            // 
+            listView1.Location = new Point(12, 12);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(955, 503);
+            listView1.TabIndex = 9;
+            listView1.UseCompatibleStateImageBehavior = false;
+            // 
             // QuanLyTaiKhoanGUI
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(853, 527);
+            ClientSize = new Size(1097, 527);
+            Controls.Add(listView1);
             Controls.Add(button3);
             Controls.Add(button1);
             Controls.Add(button2);
-            Controls.Add(dataGridView1);
             Name = "QuanLyTaiKhoanGUI";
             Text = "QuanLyTaiKhoanGUI";
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
-        private DataGridView dataGridView1;
         private Button button2;
         private Button button1;
         private Button button3;
+        private ListView listView1;
     }
 }
