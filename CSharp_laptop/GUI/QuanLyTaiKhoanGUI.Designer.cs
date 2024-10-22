c1d081bb00f74a5b7c705351dcd3ef0f6e45bc3c
Tien <tienheo1306@gmail.com>
2024-10-23 00:38:16 +0700

    update taikhoan

--- CSharp_laptop/GUI/QuanLyTaiKhoanGUI.Designer.cs
+++ CSharp_laptop/GUI/QuanLyTaiKhoanGUI.Designer.cs
@@ -28,91 +28,92 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(QuanLyTaiKhoanGUI));
-            button2 = new Button();
-            button1 = new Button();
-            button3 = new Button();
-            listView1 = new ListView();
+            label1 = new Label();
+            vbButton2 = new VBButton();
+            textBox1 = new TextBox();
+            dataGridView1 = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
-            // button2
+            // label1
             // 
-            button2.Anchor = AnchorStyles.Top | AnchorStyles.Right;
-            button2.BackColor = Color.FromArgb(0, 155, 155);
-            button2.Font = new Font("Segoe UI", 12.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            button2.ForeColor = Color.White;
-            button2.Image = (Image)resources.GetObject("button2.Image");
-            button2.ImageAlign = ContentAlignment.MiddleLeft;
-            button2.Location = new Point(973, 76);
-            button2.Name = "button2";
-            button2.Padding = new Padding(10, 0, 0, 0);
-            button2.Size = new Size(112, 58);
-            button2.TabIndex = 6;
-            button2.Text = "Xóa";
-            button2.TextImageRelation = TextImageRelation.ImageBeforeText;
-            button2.UseVisualStyleBackColor = false;
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.MediumPurple;
+            label1.Location = new Point(19, 41);
+            label1.Name = "label1";
+            label1.Size = new Size(340, 46);
+            label1.TabIndex = 10;
+            label1.Text = "Danh sách tài khoản";
             // 
-            // button1
+            // vbButton2
             // 
-            button1.Anchor = AnchorStyles.Top | AnchorStyles.Right;
-            button1.BackColor = Color.FromArgb(0, 155, 155);
-            button1.Font = new Font("Segoe UI", 12.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            button1.ForeColor = Color.White;
-            button1.Image = (Image)resources.GetObject("button1.Image");
-            button1.ImageAlign = ContentAlignment.MiddleLeft;
-            button1.Location = new Point(973, 12);
-            button1.Name = "button1";
-            button1.Padding = new Padding(10, 0, 0, 0);
-            button1.Size = new Size(112, 58);
-            button1.TabIndex = 7;
-            button1.Text = "Thêm";
-            button1.TextImageRelation = TextImageRelation.ImageBeforeText;
-            button1.UseVisualStyleBackColor = false;
-            button1.Click += button1_Click_1;
+            vbButton2.BackColor = Color.MediumSlateBlue;
+            vbButton2.BackgroundColor = Color.MediumSlateBlue;
+            vbButton2.BorderColor = Color.PaleVioletRed;
+            vbButton2.BorderRadius = 20;
+            vbButton2.BorderSize = 0;
+            vbButton2.FlatAppearance.BorderSize = 0;
+            vbButton2.FlatStyle = FlatStyle.Flat;
+            vbButton2.Font = new Font("Segoe UI", 12.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            vbButton2.ForeColor = Color.White;
+            vbButton2.Image = Properties.Resources.icons8_find_29;
+            vbButton2.ImageAlign = ContentAlignment.MiddleLeft;
+            vbButton2.Location = new Point(361, 105);
+            vbButton2.Margin = new Padding(3, 4, 3, 4);
+            vbButton2.Name = "vbButton2";
+            vbButton2.Padding = new Padding(8, 0, 0, 0);
+            vbButton2.Size = new Size(157, 59);
+            vbButton2.TabIndex = 12;
+            vbButton2.Text = "Tìm kiếm";
+            vbButton2.TextColor = Color.White;
+            vbButton2.TextImageRelation = TextImageRelation.ImageBeforeText;
+            vbButton2.UseVisualStyleBackColor = false;
             // 
-            // button3
+            // textBox1
             // 
-            button3.Anchor = AnchorStyles.Top | AnchorStyles.Right;
-            button3.BackColor = Color.FromArgb(0, 155, 155);
-            button3.Font = new Font("Segoe UI", 12.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            button3.ForeColor = Color.White;
-            button3.Image = (Image)resources.GetObject("button3.Image");
-            button3.ImageAlign = ContentAlignment.MiddleLeft;
-            button3.Location = new Point(973, 140);
-            button3.Name = "button3";
-            button3.Padding = new Padding(10, 0, 0, 0);
-            button3.Size = new Size(112, 58);
-            button3.TabIndex = 8;
-            button3.Text = "Lưu";
-            button3.TextImageRelation = TextImageRelation.ImageBeforeText;
-            button3.UseVisualStyleBackColor = false;
+            textBox1.Font = new Font("Segoe UI", 14.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(19, 123);
+            textBox1.Margin = new Padding(3, 4, 3, 4);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(316, 39);
+            textBox1.TabIndex = 11;
             // 
-            // listView1
+            // dataGridView1
             // 
-            listView1.Location = new Point(12, 12);
-            listView1.Name = "listView1";
-            listView1.Size = new Size(955, 503);
-            listView1.TabIndex = 9;
-            listView1.UseCompatibleStateImageBehavior = false;
+            dataGridView1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            dataGridView1.BackgroundColor = Color.White;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.GridColor = Color.Black;
+            dataGridView1.Location = new Point(13, 192);
+            dataGridView1.Margin = new Padding(3, 4, 3, 4);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 62;
+            dataGridView1.Size = new Size(1374, 735);
+            dataGridView1.TabIndex = 14;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
             // 
             // QuanLyTaiKhoanGUI
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1097, 527);
-            Controls.Add(listView1);
-            Controls.Add(button3);
-            Controls.Add(button1);
-            Controls.Add(button2);
+            ClientSize = new Size(1405, 937);
+            Controls.Add(dataGridView1);
+            Controls.Add(vbButton2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "QuanLyTaiKhoanGUI";
             Text = "QuanLyTaiKhoanGUI";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
-        private Button button2;
-        private Button button1;
-        private Button button3;
-        private ListView listView1;
+        private Label label1;
+        private VBButton vbButton2;
+        private TextBox textBox1;
+        private DataGridView dataGridView1;
     }
 }
