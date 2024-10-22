89c4916df7c32871598fe73543939df3e79f2e76
duyKhanh026 <117454177+duyKhanh026@users.noreply.github.com>
2024-10-22 10:33:57 +0700

    duma

---
+++ CSharp_laptop/GUI/NhanVien/NhanVienGUI.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace CSharp_laptop.GUI
+{
+    partial class NhanVienGUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            label1 = new Label();
+            button3 = new Button();
+            dataGridView1 = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 14.25F);
+            textBox1.Location = new Point(15, 70);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(422, 33);
+            textBox1.TabIndex = 11;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 20.25F, FontStyle.Bold);
+            label1.ForeColor = Color.MediumPurple;
+            label1.Location = new Point(32, 18);
+            label1.Name = "label1";
+            label1.Size = new Size(278, 37);
+            label1.TabIndex = 10;
+            label1.Text = "Danh sách nhân viên";
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button3.ForeColor = Color.FromArgb(249, 118, 176);
+            button3.Location = new Point(467, 63);
+            button3.Name = "button3";
+            button3.Size = new Size(110, 44);
+            button3.TabIndex = 12;
+            button3.Text = "Tìm kiếm";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            dataGridView1.BackgroundColor = SystemColors.Control;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(17, 118);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(984, 489);
+            dataGridView1.TabIndex = 13;
+            // 
+            // NhanVienGUI
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1013, 619);
+            Controls.Add(dataGridView1);
+            Controls.Add(button3);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Margin = new Padding(3, 2, 3, 2);
+            Name = "NhanVienGUI";
+            Text = "NhanVienGUI";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private TextBox textBox1;
+        private Label label1;
+        private Button button3;
+        private DataGridView dataGridView1;
+    }
+}
