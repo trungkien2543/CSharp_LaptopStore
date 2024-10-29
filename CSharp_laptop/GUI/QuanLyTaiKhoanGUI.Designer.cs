07d3262d80aa8b48a7d0a1ba236f6b608f092e9e
Tien <tienheo1306@gmail.com>
2024-10-29 14:33:15 +0700

    update taikhoan

--- CSharp_laptop/GUI/QuanLyTaiKhoanGUI.Designer.cs
+++ CSharp_laptop/GUI/QuanLyTaiKhoanGUI.Designer.cs
@@ -28,27 +28,21 @@
         /// </summary>
         private void InitializeComponent()
         {
-            dataGridView1 = new DataGridView();
+            DataGridViewCellStyle dataGridViewCellStyle1 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle3 = new DataGridViewCellStyle();
             artanPanel1 = new ArtanPanel();
             vbButton1 = new VBButton();
             label1 = new Label();
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            dataGridView1 = new DataGridView();
+            Column2 = new DataGridViewTextBoxColumn();
+            Column3 = new DataGridViewTextBoxColumn();
+            Column1 = new DataGridViewTextBoxColumn();
+            Column7 = new DataGridViewTextBoxColumn();
+            Column8 = new DataGridViewTextBoxColumn();
             artanPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
-            // 
-            // dataGridView1
-            // 
-            dataGridView1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-            dataGridView1.BackgroundColor = Color.White;
-            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.GridColor = Color.Black;
-            dataGridView1.Location = new Point(12, 177);
-            dataGridView1.Margin = new Padding(3, 4, 3, 4);
-            dataGridView1.Name = "dataGridView1";
-            dataGridView1.RowHeadersWidth = 62;
-            dataGridView1.Size = new Size(1374, 747);
-            dataGridView1.TabIndex = 14;
-            dataGridView1.CellContentClick += dataGridView1_CellContentClick;
             // 
             // artanPanel1
             // 
@@ -59,19 +53,20 @@
             artanPanel1.Controls.Add(label1);
             artanPanel1.ForeColor = Color.Black;
             artanPanel1.GradientAngle = 30F;
-            artanPanel1.GradientBottomColor = Color.CadetBlue;
+            artanPanel1.GradientBottomColor = Color.MediumPurple;
             artanPanel1.GradientTopColor = Color.DodgerBlue;
             artanPanel1.Location = new Point(12, 13);
             artanPanel1.Margin = new Padding(3, 4, 3, 4);
             artanPanel1.Name = "artanPanel1";
             artanPanel1.Size = new Size(1374, 156);
             artanPanel1.TabIndex = 15;
+            artanPanel1.Paint += artanPanel1_Paint_1;
             // 
             // vbButton1
             // 
             vbButton1.Anchor = AnchorStyles.Top | AnchorStyles.Right;
-            vbButton1.BackColor = Color.MediumSlateBlue;
-            vbButton1.BackgroundColor = Color.MediumSlateBlue;
+            vbButton1.BackColor = Color.RoyalBlue;
+            vbButton1.BackgroundColor = Color.RoyalBlue;
             vbButton1.BorderColor = Color.PaleVioletRed;
             vbButton1.BorderRadius = 20;
             vbButton1.BorderSize = 0;
@@ -81,16 +76,17 @@
             vbButton1.ForeColor = Color.White;
             vbButton1.Image = Properties.Resources.icons8_add_29;
             vbButton1.ImageAlign = ContentAlignment.MiddleLeft;
-            vbButton1.Location = new Point(1211, 43);
+            vbButton1.Location = new Point(1195, 46);
             vbButton1.Margin = new Padding(3, 4, 3, 4);
             vbButton1.Name = "vbButton1";
             vbButton1.Padding = new Padding(8, 0, 0, 0);
-            vbButton1.Size = new Size(141, 77);
-            vbButton1.TabIndex = 7;
+            vbButton1.Size = new Size(147, 70);
+            vbButton1.TabIndex = 11;
             vbButton1.Text = "Thêm";
             vbButton1.TextColor = Color.White;
             vbButton1.TextImageRelation = TextImageRelation.ImageBeforeText;
             vbButton1.UseVisualStyleBackColor = false;
+            vbButton1.Click += vbButton1_Click;
             // 
             // label1
             // 
@@ -100,30 +96,134 @@
             label1.ForeColor = Color.White;
             label1.Location = new Point(37, 55);
             label1.Name = "label1";
-            label1.Size = new Size(363, 46);
+            label1.Size = new Size(340, 46);
             label1.TabIndex = 10;
-            label1.Text = "Danh sách loại laptop";
+            label1.Text = "Danh sách tài khoản";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
+            dataGridView1.AllowUserToResizeRows = false;
+            dataGridView1.BackgroundColor = Color.White;
+            dataGridView1.BorderStyle = BorderStyle.None;
+            dataGridView1.CellBorderStyle = DataGridViewCellBorderStyle.None;
+            dataGridView1.ClipboardCopyMode = DataGridViewClipboardCopyMode.EnableWithoutHeaderText;
+            dataGridView1.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = Color.FromArgb(113, 96, 232);
+            dataGridViewCellStyle1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            dataGridViewCellStyle1.ForeColor = Color.FromArgb(235, 230, 255);
+            dataGridViewCellStyle1.SelectionBackColor = Color.FromArgb(113, 96, 232);
+            dataGridViewCellStyle1.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = DataGridViewTriState.True;
+            dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            dataGridView1.ColumnHeadersHeight = 35;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { Column2, Column3, Column1, Column7, Column8 });
+            dataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = Color.White;
+            dataGridViewCellStyle2.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            dataGridViewCellStyle2.ForeColor = Color.FromArgb(100, 150, 200);
+            dataGridViewCellStyle2.SelectionBackColor = Color.FromArgb(235, 230, 255);
+            dataGridViewCellStyle2.SelectionForeColor = Color.FromArgb(100, 100, 180);
+            dataGridViewCellStyle2.WrapMode = DataGridViewTriState.False;
+            dataGridView1.DefaultCellStyle = dataGridViewCellStyle2;
+            dataGridView1.EnableHeadersVisualStyles = false;
+            dataGridView1.GridColor = Color.LightGray;
+            dataGridView1.Location = new Point(12, 177);
+            dataGridView1.Margin = new Padding(3, 4, 3, 4);
+            dataGridView1.MultiSelect = false;
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.ReadOnly = true;
+            dataGridViewCellStyle3.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = SystemColors.Control;
+            dataGridViewCellStyle3.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            dataGridViewCellStyle3.ForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle3.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = DataGridViewTriState.True;
+            dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            dataGridView1.RowHeadersVisible = false;
+            dataGridView1.RowHeadersWidth = 25;
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dataGridView1.Size = new Size(1258, 683);
+            dataGridView1.TabIndex = 0;
+            dataGridView1.CellContentClick += dataGridView1_CellContentClick_1;
+            dataGridView1.CellPainting += dataGridView1_CellPainting;
+            dataGridView1.RowsAdded += dataGridView1_RowsAdded;
+            dataGridView1.SizeChanged += dataGridView1_SizeChanged;
+            // 
+            // Column2
+            // 
+            Column2.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            Column2.FillWeight = 80F;
+            Column2.HeaderText = "Tên tài khoản";
+            Column2.MinimumWidth = 6;
+            Column2.Name = "Column2";
+            Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            Column3.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            Column3.FillWeight = 70F;
+            Column3.HeaderText = "Mật khẩu";
+            Column3.MinimumWidth = 6;
+            Column3.Name = "Column3";
+            Column3.ReadOnly = true;
+            // 
+            // Column1
+            // 
+            Column1.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            Column1.FillWeight = 50F;
+            Column1.HeaderText = "Quyền";
+            Column1.MinimumWidth = 6;
+            Column1.Name = "Column1";
+            Column1.ReadOnly = true;
+            // 
+            // Column7
+            // 
+            Column7.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            Column7.FillWeight = 40F;
+            Column7.HeaderText = "Sửa";
+            Column7.MinimumWidth = 6;
+            Column7.Name = "Column7";
+            Column7.ReadOnly = true;
+            // 
+            // Column8
+            // 
+            Column8.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            Column8.FillWeight = 40F;
+            Column8.HeaderText = "Xóa";
+            Column8.MinimumWidth = 6;
+            Column8.Name = "Column8";
+            Column8.ReadOnly = true;
             // 
             // QuanLyTaiKhoanGUI
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1405, 937);
-            Controls.Add(artanPanel1);
             Controls.Add(dataGridView1);
+            Controls.Add(artanPanel1);
             Margin = new Padding(3, 4, 3, 4);
             Name = "QuanLyTaiKhoanGUI";
             Text = "QuanLyTaiKhoanGUI";
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            Load += TaiKhoanGUI_Load;
             artanPanel1.ResumeLayout(false);
             artanPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
-        private DataGridView dataGridView1;
         private ArtanPanel artanPanel1;
-        private VBButton vbButton1;
         private Label label1;
+        private DataGridView dataGridView1;
+        private VBButton vbButton1;
+        private DataGridViewTextBoxColumn Column2;
+        private DataGridViewTextBoxColumn Column3;
+        private DataGridViewTextBoxColumn Column1;
+        private DataGridViewTextBoxColumn Column7;
+        private DataGridViewTextBoxColumn Column8;
     }
 }
--- CSharp_laptop/GUI/QuanLyTaiKhoanGUI.cs
+++ CSharp_laptop/GUI/QuanLyTaiKhoanGUI.cs
@@ -1,6 +1,7 @@
 ﻿using CSharp_laptop.BUS;
 using CSharp_laptop.DTO;
 using CSharp_laptop.GUI.Laptop;
+using CSharp_laptop.Properties;
 using LaptopStore.DTO;
 using System;
 using System.Collections.Generic;
@@ -8,6 +9,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Resources;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -16,26 +18,51 @@ namespace CSharp_laptop.GUI
 {
     public partial class QuanLyTaiKhoanGUI : Form
     {
-        private TaiKhoanBUS TaiKhoanBUS = new TaiKhoanBUS();
-        private MainForm mainForm;
-        string selectedTK;
-        string soluong_tk;
+        //private TaiKhoanBUS TaiKhoanBUS = new TaiKhoanBUS();
+        //private MainForm mainForm;
+        //string selectedTK;
+        //string soluong_tk;
+
+        MainForm mainForm;
+        TaiKhoanBUS bus;
+        List<TaiKhoanDTO> nvs;
+        List<VBButton> btnEditList;
+        List<VBButton> btnDelList;
         public QuanLyTaiKhoanGUI(MainForm mainForm)
         {
+            //this.mainForm = mainForm;
+            //InitializeComponent();
+            //LoadTaiKhoan();
+            //AddButtonsToDataGridView();
+            //edittable();
+
+            bus = new TaiKhoanBUS();
+            nvs = bus.GetAllTaiKhoan();
+
+            btnEditList = new List<VBButton>();
+            btnDelList = new List<VBButton>();
+
             this.mainForm = mainForm;
             InitializeComponent();
-            LoadTaiKhoan();
-            AddButtonsToDataGridView();
-            edittable();
+
         }
 
         private void LoadTaiKhoan()
         {
 
-            List<TaiKhoanDTO> tk = TaiKhoanBUS.GetAllTaiKhoan();
-            soluong_tk = (1 + tk.Count).ToString();
+            //List<TaiKhoanDTO> tk = TaiKhoanBUS.GetAllTaiKhoan();
+            //soluong_tk = (1 + tk.Count).ToString();
 
-            dataGridView1.DataSource = tk;
+            //dataGridView1.DataSource = tk;
+
+        }
+        private void TaiKhoanGUI_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < nvs.Count; i++)
+            {
+                TaiKhoanDTO nv = nvs[i];
+                dataGridView1.Rows.Add(new object[] { nv.TenDN, nv.MatKhau, nv.Quyen });
+            }
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -57,32 +84,93 @@ namespace CSharp_laptop.GUI
         {
 
         }
-
-        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        private bool IsbtnEditListNameExist(string name)
         {
-            if (e.RowIndex >= 0)
+            // Kiểm tra xem có Button nào trong danh sách có Name khớp với tên cần tìm không
+            return btnEditList.Any(btn => btn.Name == name);
+        }
+        private bool IsbtnDelListNameExist(string name)
+        {
+            // Kiểm tra xem có Button nào trong danh sách có Name khớp với tên cần tìm không
+            return btnDelList.Any(btn => btn.Name == name);
+        }
+        private void BtnEdit_Click(object sender, EventArgs e)
+        {
+            Button clickedButton = sender as Button;
+            if (clickedButton != null)
             {
+                dataGridView1.Rows[int.Parse(clickedButton.Name)].Selected = true;
+            }
+        }
+        private void BtnDel_Click(object sender, EventArgs e)
+        {
+            Button clickedButton = sender as Button;
+            if (clickedButton != null)
+            {
+                dataGridView1.Rows[int.Parse(clickedButton.Name)].Selected = true;
+            }
+        }
+        private void dataGridView1_CellPainting(object sender, DataGridViewCellPaintingEventArgs e)
+        {
+            if (e.ColumnIndex == dataGridView1.Columns["Column7"].Index && e.RowIndex >= 0)
+            {
+                if (!IsbtnEditListNameExist(e.RowIndex.ToString()))
+                {
+                    // Tạo nút VBButton cho mỗi hàng
+                    VBButton btn = new VBButton();
+                    btn.Size = new Size(e.CellBounds.Width, e.CellBounds.Height);
+                    btn.Location = new Point(e.CellBounds.X, e.CellBounds.Y);
+                    btn.BorderRadius = 10;
 
-                DataGridViewRow row = dataGridView1.Rows[e.RowIndex];
-                string idLaptop = row.Cells["TenDN"].Value.ToString();
-                selectedTK = idLaptop;
+                    btn.Name = e.RowIndex.ToString();
+                    btn.Text = "✏️";
+
+                    // Thêm sự kiện click cho VBButton nếu cần
+                    btn.Click += BtnEdit_Click;
+
+                    // Thêm nút vào control của DataGridView
+                    dataGridView1.Controls.Add(btn);
+                    btnEditList.Add(btn);
+
+                    // Đánh dấu đã vẽ ô để tránh vẽ đè
+                    e.Handled = true;
+                }
+                else
+                {
+                    btnEditList[e.RowIndex].Size = new Size(e.CellBounds.Width, e.CellBounds.Height);
+                    btnEditList[e.RowIndex].Location = new Point(e.CellBounds.X, e.CellBounds.Y);
+                }
 
             }
-
-            if (e.ColumnIndex == dataGridView1.Columns["btnEdit"].Index && e.RowIndex >= 0)
+            if (e.ColumnIndex == dataGridView1.Columns["Column8"].Index && e.RowIndex >= 0)
             {
-                //mainForm.OpenChildForm(new EditSanPham(selectedTK, "Sửa sản phẩm", mainForm));
-            }
-            else if (e.ColumnIndex == dataGridView1.Columns["btnDelete"].Index && e.RowIndex >= 0)
-            {
+                if (!IsbtnDelListNameExist(e.RowIndex.ToString()))
+                {
+                    // Tạo nút VBButton cho mỗi hàng
+                    VBButton btn = new VBButton();
+                    btn.Size = new Size(e.CellBounds.Width, e.CellBounds.Height);
+                    btn.Location = new Point(e.CellBounds.X, e.CellBounds.Y);
+                    btn.BorderRadius = 10;
 
-                DialogResult result = MessageBox.Show($"Bạn có chắc chắn muốn xóa sản phẩm với ID: {selectedTK}?", "Xóa", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                    btn.Name = e.RowIndex.ToString();
+                    btn.Text = "❌";
 
+                    // Thêm sự kiện click cho VBButton nếu cần
+                    btn.Click += BtnDel_Click;
 
-            }
-            else if (e.ColumnIndex == dataGridView1.Columns["btnThem"].Index && e.RowIndex >= 0)
-            {
-                //mainForm.OpenChildForm(new QuanLyTaiKhoanGUI(selectedTK, mainForm));
+                    // Thêm nút vào control của DataGridView
+                    dataGridView1.Controls.Add(btn);
+                    btnDelList.Add(btn);
+
+                    // Đánh dấu đã vẽ ô để tránh vẽ đè
+                    e.Handled = true;
+                }
+                else
+                {
+                    btnDelList[e.RowIndex].Size = new Size(e.CellBounds.Width, e.CellBounds.Height);
+                    btnDelList[e.RowIndex].Location = new Point(e.CellBounds.X, e.CellBounds.Y);
+                }
+
             }
         }
         private void AddButtonsToDataGridView()
@@ -132,6 +220,31 @@ namespace CSharp_laptop.GUI
         }
 
         private void vbButton2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick_1(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_RowsAdded(object sender, DataGridViewRowsAddedEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_SizeChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void artanPanel1_Paint_1(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void vbButton1_Click(object sender, EventArgs e)
         {
 
         }
